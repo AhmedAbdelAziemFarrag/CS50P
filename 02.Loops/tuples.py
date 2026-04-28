98e4d85044f470121280b8c7dd0f78ef1171cd9f
Ahmed Abdel-Aziem Farrag <ahm.aziem.farrag@gmail.com>
2026-04-28 06:16:26 +0400

    Update

---
+++ 02.Loops/tuples.py
@@ -0,0 +1,14 @@
+def main():
+    coordinates = (42.376, -71.115)
+    print(f"Latitude: {coordinates[0]}")
+    print(f"Longitude: {coordinates[1]}")
+    
+    latitude, longitude = coordinates
+    print(f"Latitude: {latitude}")
+    print(f"Longitude: {longitude}")
+    
+    
+
+
+
+main()
