45b159e501b1e5cb6000c88e61485e22701d27e6
Ahmed Abdel-Aziem Farrag <ahm.aziem.farrag@gmail.com>
2026-04-28 05:55:41 +0400

    update

--- 02.Loops/phone.py
+++ 02.Loops/phone.py
@@ -1,6 +1,13 @@
 def main():
     phone = "617-495-1000"
-    print(phone)
+    print(phone[0:3])
+    print(phone[:3])
+    print(phone[8:12])
+    print(phone[8:])
+    
+    phone = "+1 617-495-1000"
+    print(phone[-4:])
+    print(phone[:-4])
     
 
 main()
